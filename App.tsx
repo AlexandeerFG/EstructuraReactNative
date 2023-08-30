8813e6b59fa82e151c4be223c9e4c690dc9cb7fc
fogoso <alefragar9@gmail.com>
2023-08-30 16:30:17 -0600

    cambios

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {Image, Text, View} from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 export const App = () => {
   return (
@@ -6,19 +6,23 @@ export const App = () => {
       {/* <Text>Pinche Marvin Chaquetin</Text> */}
       <Image
         source={{uri: 'https://reqres.in/img/faces/1-image.jpg'}}
-        style={{width: 150, height: 150, borderRadius: 100}}
+        style={{width: 120, height: 120, borderRadius: 100}}
       />
       <Image
         source={{uri: 'https://reqres.in/img/faces/2-image.jpg'}}
-        style={{width: 150, height: 150, borderRadius: 100}}
+        style={{width: 120, height: 120, borderRadius: 100}}
       />
-       <Image
+      <Image
         source={{uri: 'https://reqres.in/img/faces/3-image.jpg'}}
-        style={{width: 150, height: 150, borderRadius: 100}}
+        style={{width: 120, height: 120, borderRadius: 100}}
       />
       <Image
         source={{uri: 'https://reqres.in/img/faces/4-image.jpg'}}
-        style={{width: 150, height: 150, borderRadius: 100}}
+        style={{width: 120, height: 120, borderRadius: 100}}
+      />
+      <Image
+        source={{uri: 'https://reqres.in/img/faces/5-image.jpg'}}
+        style={{width: 120, height: 120, borderRadius: 100}}
       />
     </View>
   );
